2b593fb45bac0d53bdb788a91da70c1e854df62b
dsfomin <dsfomin2@gmail.com>
2022-04-11 14:08:11 +0200

    UI improving

--- src/js/src/app/app.component.ts
+++ src/js/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component} from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { TextAnalyze } from './text-analyze';
 import { TextAnalyzeService } from './text-analyze.service';
 
@@ -14,13 +15,18 @@ export class AppComponent {
   public text: string;
   public mode: string;
   public connection: string;
+  public loading: boolean;
+  public inputFormControl: FormControl;
+  
 
   constructor(private textAnalyzeService: TextAnalyzeService) {
     this.analyze = new Map();
     this.textToAnalyze = new TextAnalyze();
     this.text = '';
-    this.mode = '';
-    this.connection = '';
+    this.mode = 'vowels';
+    this.connection = 'offline';
+    this.loading = false;
+    this.inputFormControl = new FormControl('', [Validators.required])
   }
 
   public getMap(): void {
@@ -28,8 +34,10 @@ export class AppComponent {
     this.textToAnalyze.text = this.text;
 
     if (this.connection === 'online') {
+      this.loading = true;
       this.textAnalyzeService.getMapOfVowelsOrConsonantsOnline(this.textToAnalyze).subscribe(
         (response: Map<string, number>) => {
+          this.loading = false;
           this.analyze = response;
         },
         (error: HttpErrorResponse) => {
